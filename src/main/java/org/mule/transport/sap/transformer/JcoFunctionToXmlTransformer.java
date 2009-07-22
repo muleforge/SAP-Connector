02953f9f070d8cf0cb4ef0c32f7a53fe3b5cc69b
yyamano <yyamano@4e1bfb89-7866-0410-8257-83b4286908d9>
2009-07-22 19:03:01 +0000

    Add copyright notice (copied from 2smule). Add and enable Id keyword.

--- src/main/java/org/mule/transport/sap/transformer/JcoFunctionToXmlTransformer.java
+++ src/main/java/org/mule/transport/sap/transformer/JcoFunctionToXmlTransformer.java
@@ -1,3 +1,13 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) Osaka Gas Information System Research Institute Co., Ltd.
+ * All rights reserved.  http://www.ogis-ri.co.jp/
+ * 
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.transport.sap.transformer;
 
 import java.util.Date;
